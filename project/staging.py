10dec2c7ea9c3ce82c527becf9ef9ed68cca8f7e
kodexArg <gcavedal@gmail.com>
2024-09-10 22:56:13 +0000

    staging ahora acepta DEBUG True

--- project/staging.py
+++ project/staging.py
@@ -1,41 +1,54 @@
 from .base import *
 from environs import Env
+from loguru import logger
 import os
 
 env = Env()
 env.read_env()
 
-DEBUG = False
+# Activamos el DEBUG temporalmente para obtener más detalles
+DEBUG = env.bool('DEBUG', default=False)
 
+# Permitimos cualquier host en este entorno de staging
 ALLOWED_HOSTS = ['*']
 
-# Cambios de configuración de archivos estáticos y de medios
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+# Habilitamos logs de que estamos en staging
+logger.info("[INFO] Usando configuración de staging.py")
 
-STORAGES = {
-    'default': {
-        'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
-        'OPTIONS': {
-            'bucket_name': AWS_STORAGE_BUCKET_NAME,
-            'location': 'media',
+# Configuración de archivos estáticos y de medios
+if DEBUG:
+    # Modo DEBUG - Utilizando recursos locales
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [BASE_DIR / 'core/static']
+
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
+    logger.info(f"Using DEBUG MODE:\n - MEDIA_ROOT={MEDIA_ROOT}\n - STATICFILES_DIRS={STATICFILES_DIRS}\n")
+
+else:
+    # Configuración de producción en S3
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+    STORAGES = {
+        'default': {
+            'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
+            'OPTIONS': {
+                'bucket_name': AWS_STORAGE_BUCKET_NAME,
+                'location': 'media',
+            },
         },
-    },
-    'staticfiles': {
-        'BACKEND': 'storages.backends.s3boto3.S3StaticStorage',
-        'OPTIONS': {
-            'bucket_name': AWS_STORAGE_BUCKET_NAME,
-            'location': 'static',
+        'staticfiles': {
+            'BACKEND': 'storages.backends.s3boto3.S3StaticStorage',
+            'OPTIONS': {
+                'bucket_name': AWS_STORAGE_BUCKET_NAME,
+                'location': 'static',
+            },
         },
-    },
-}
+    }
 
-# Parámetros de almacenamiento S3 (cache de un día)
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',  
-}
-
-# Base de datos para el entorno de Staging (usa las mismas variables que Development)
+# Configuración de la base de datos
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -46,3 +59,35 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
+# Parámetros de almacenamiento S3
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',  # Cache de un día
+}
+
+# Configuración del logging para capturar más detalles durante DEBUG
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'debug.log',  # Archivo donde se guardarán los logs
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG' if DEBUG else 'INFO',
+            'propagate': True,
+        },
+        'django.request': {  # Captura errores de requests HTTP
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
+
+logger.info("[INFO] Finalizó la carga de staging.py con éxito")
